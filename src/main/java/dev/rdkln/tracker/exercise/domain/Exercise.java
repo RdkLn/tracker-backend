ad208302be5dbdd2f2230749830d6d5aad0a7bd4
rdk <radlek19@gmail.com>
2025-08-03 20:17:38 +0200

    Updated schema creation liquibase yaml

--- src/main/java/dev/rdkln/tracker/exercise/domain/Exercise.java
+++ src/main/java/dev/rdkln/tracker/exercise/domain/Exercise.java
@@ -10,6 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Data
@@ -20,6 +21,7 @@ public class Exercise {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany
     private List<ExerciseSet> sets;
 
     private String tips;
