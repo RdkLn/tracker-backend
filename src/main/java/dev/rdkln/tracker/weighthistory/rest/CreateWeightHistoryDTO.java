7bafc48d1f6e8a9dfc380a5921af5b81bef605c6
rdk <radlek19@gmail.com>
2025-08-25 00:12:58 +0200

    Removed duplicate code

--- src/main/java/dev/rdkln/tracker/weighthistory/rest/CreateWeightHistoryDTO.java
+++
@@ -1,7 +0,0 @@
-package dev.rdkln.tracker.weighthistory.rest;
-
-import java.time.LocalDate;
-
-public record CreateWeightHistoryDTO(LocalDate date, Double weight) {
-
-}
--- src/main/java/dev/rdkln/tracker/weighthistory/rest/ViewWeightHistoryDTO.java
+++
@@ -1,7 +0,0 @@
-package dev.rdkln.tracker.weighthistory.rest;
-
-import java.time.LocalDate;
-
-public record ViewWeightHistoryDTO(Double weight, LocalDate weighInDate) {
-
-}
